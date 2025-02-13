1fbba0c7384e3e5a77275ee920f0955a094fd488
Matt Meyer <mattjacobnyc@gmail.com>
2025-02-13 14:58:38 -0500

    remove @appollo-sandbox from configurations included in plugins.js to try and resolve deploy failure

--- config/plugins.js
+++ config/plugins.js
@@ -12,20 +12,6 @@ module.exports = () => ({
       },
     },
   },
-  "@apollo-sandbox": {
-    // enables the plugin only in development mode
-    // if you also want to use it in production, set this to true
-    // keep in mind that graphql playground has to be enabled
-    // enabled: process.env.NODE_ENV === "production" ? false : true,
-    enabled: true,
-    // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
-    // cors: {
-    //   credentials: false,
-    //   headers: ['Content-Type', 'Accept'],
-    //   origin: 'http://localhost:1337',
-    // },
-    mode: "no-cors",
-  },
   email: {
     config: {
       provider: "strapi-provider-email-brevo",
